2f5c654049941884c0ff3442533b8c9f0beff15a
Jake <jaygarza1982@gmail.com>
2021-03-28 20:47:49 -0400

    Added file listing in server, added react front end template.

--- server/routes/api/files.js
+++ server/routes/api/files.js
@@ -1,8 +1,31 @@
 var express = require('express');
 var router = express.Router();
+const fs = require('fs');
 
 router.get('/', (req, res) => {
-    res.send('This is the route that would return a list of files.');
+    try {
+        let dirList = fs.readdirSync(process.env.MEDIA_FILES_PATH);
+        let filesAndDirs = [];
+
+        //Build directory listing with metadata about the file
+        dirList.forEach(listing => {
+            let listingStats = fs.lstatSync(`/app/${process.env.MEDIA_FILES_PATH}/${listing}`);
+
+            filesAndDirs.push({
+                filename: listing,
+                directory: listingStats.isDirectory(),
+                size: listingStats.size,
+                created: listingStats.birthtime,
+            });
+        });
+
+        res.send(filesAndDirs);
+    } catch (err) {
+        const errorMessage = `Error reading files ${err}`;
+
+        console.log(errorMessage);
+        res.status(500).send(errorMessage);
+    }
 });
 
 module.exports = router;
