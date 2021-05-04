835cfed1ff2cbe29df167e5da0250873475f2298
Jake <jaygarza1982@gmail.com>
2021-05-03 20:27:18 -0400

    Added login page.

--- web/src/App.js
+++ web/src/App.js
@@ -3,12 +3,14 @@ import FileList from './components/FileListing';
 import { Route, Switch } from 'react-router-dom';
 import Navbar from './Navbar';
 import VideoViewer from './components/Video';
+import Login from 'components/Login';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Switch>
+        <Route path='/login' component={Login} />
         <Route path='/video/:video+' component={VideoViewer} />
         <Route path='/:folder+' component={FileList} />
         <Route path='/' component={FileList} />
